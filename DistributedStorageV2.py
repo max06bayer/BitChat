b4b1b7e580d4b56491308158d75482096f14f47f
max06bayer <max06.bayer@gmail.com>
2025-04-02 17:52:12 +0200

    Improvements: 92s bootstrapping time -> 0.3s bootstrapping time

--- DistributedStorageV2.py
+++ DistributedStorageV2.py
@@ -20,7 +20,7 @@ class file_system_node:
         bootstrap_task = asyncio.create_task(self.bootstrap(bootstrap_nodes))
         await asyncio.gather(server_task, bootstrap_task)
 
-    def get_local_ip(self):
+    def get_local_ip(self) -> str:
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         server.connect(('8.8.8.8', 80)); local_ip = server.getsockname()[0];
         server.close(); return local_ip
@@ -34,12 +34,15 @@ class file_system_node:
     async def handle_connection(self, reader, writer):
         data_raw = await reader.read(1024)
         data = json.loads(data_raw.decode("utf-8"))
-        client_address = writer.get_extra_info('peername')
 
         if 'node_info_request' in data:
-            print(f"Received node info request from {client_address}")
             self.add_node(data['node_info_request'][0], data['node_info_request'][1])
             response = {'node_info_response': (self.public_ip, self.CID)}
+            writer.write(json.dumps(response).encode())
+            await writer.drain()
+        elif 'closest_nodes_request' in data:
+            closest_nodes = self.get_closest_nodes(data['closest_nodes_request'][1])
+            response = {'closest_nodes_response': closest_nodes}
             writer.write(json.dumps(response).encode())
             await writer.drain()
 
@@ -52,7 +55,11 @@ class file_system_node:
         if 'node_info_request' in data:
             response_data = await asyncio.wait_for(reader.read(1024), timeout)
             response = json.loads(response_data.decode())['node_info_response']
-            print(f"Received node info response from {writer.get_extra_info('peername')}")
+            writer.close(); await writer.wait_closed()
+            return response
+        elif 'closest_nodes_request' in data:
+            response_data = await asyncio.wait_for(reader.read(1024), timeout)
+            response = json.loads(response_data.decode())['closest_nodes_response']
             writer.close(); await writer.wait_closed()
             return response
         else: writer.close(); await writer.wait_closed(); return None
@@ -66,9 +73,52 @@ class file_system_node:
             new_node = await self.send_data(node, {'node_info_request': (self.public_ip, self.CID)})
             self.add_node(new_node[0], new_node[1])
 
-        while True: await asyncio.sleep(1)
+        while True: 
+            for index, bucket in enumerate(bucket_targets):
+                closest_nodes = await self.deep_node_search(bucket)
+                for node in closest_nodes:
+                    if self.get_bucket_index(node[0]) == index: 
+                        self.add_node(node[1], node[0])
+            await asyncio.sleep(1)
 
-    def get_bucket_index(self, cid: str):
+    async def deep_node_search(self, cid: str, amount=4) -> list:
+        closest_nodes = []; asked_nodes = []
+        closest_nodes = self.get_closest_nodes(cid)
+        closest_distance = float('inf')
+
+        while True:
+            next_to_ask = None
+            for node in closest_nodes:
+                if node not in asked_nodes:
+                    distance = int(int(node[0], base=16) ^ int(cid, base=16)).bit_length() - 1
+                    if distance < closest_distance:
+                        next_to_ask = node
+                        closest_distance = distance
+            
+            if not next_to_ask: break
+            asked_nodes.append(next_to_ask)
+            new_nodes = await self.ask_for_closest_nodes(next_to_ask[1], cid)
+            all_nodes = closest_nodes + new_nodes
+            all_nodes.sort(key=lambda node: int(node[0], base=16) ^ int(cid, base=16))
+            closest_nodes = all_nodes[:amount]
+            new_closest = int(closest_nodes[0][0], base=16) ^ int(cid, base=16)
+            if new_closest >= closest_distance: break
+            closest_distance = new_closest
+        return closest_nodes[:amount]
+
+    async def ask_for_closest_nodes(self, target_node_ip, cid: str) -> list:
+        return await self.send_data(target_node_ip, {'closest_nodes_request': (self.public_ip, cid)})
+
+    def get_closest_nodes(self, cid, amount=4) -> list:
+        all_nodes = []
+        for bucket in self.DHT:
+            for node in bucket:
+                all_nodes.append(node)
+
+        all_nodes.sort(key=lambda node: int(node[0], base=16) ^ int(cid, base=16))
+        return all_nodes[:amount]
+
+    def get_bucket_index(self, cid: str) -> int:
         hash_0 = int(self.CID, base=16)
         hash_1 = int(cid, base=16)
         distance = hash_0 ^ hash_1
@@ -76,6 +126,8 @@ class file_system_node:
 
     def add_node(self, ip: str, cid: str):
         dht_index = self.get_bucket_index(cid)
+        if (cid, ip) in self.DHT[dht_index]: return
+        print(f"Found Node {ip} in bucket {dht_index}")
         self.DHT[dht_index].append((cid,ip))
     
     def generate_target_cid(self, bucketDistance: int) -> str:
